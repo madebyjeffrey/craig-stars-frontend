a7dafdbffe8876c01caef0eb4f3abdc9a7c9ecd0
sirgwain <sirgwain@gmail.com>
2023-06-18 18:04:14 -0700

    lots of battle cleanup battle viewer implemented

--- frontend/src/routes/(user)/tests/battle/+layout@.svelte
+++ frontend/src/routes/(user)/tests/battle/+layout@.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
 	import Menu from '$lib/components/Menu.svelte';
 	import { me } from '$lib/services/Context';
-	import '../../css/app.css';
-	import '../../css/hulls.css';
-	import '../../css/planets.css';
-	import '../../css/techs.css';
 </script>
 
 <main class="p-3 flex flex-col">
 	<div class="sticky top-0 z-10">
 		<Menu user={$me} />
 	</div>
-	<div class="w-full mx-auto md:max-w-2xl">
-		<slot>This is the main content</slot>
+	<div class="relative">
+		<div class="w-full mx-auto">
+			<slot>This is the main content</slot>
+		</div>
 	</div>
 </main>
